641dbc0bcdbd2e58a51bb1e9340804a66fed53a1
Gunvich <gunderovich.work@gmail.com>
2025-12-18 17:14:22 +0200

    feat: add ticket model and migration

--- src/database/migrations/2025_12_18_151024_create_tickets_table.php
+++ src/database/migrations/2025_12_18_151024_create_tickets_table.php
@@ -11,11 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('tickets', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->nullable();
-            $table->string('phone')->unique();
+            $table->foreignId('customer_id')->constrained()->cascadeOnDelete();
+            $table->string('subject');
+            $table->text('message');
+            $table->string('status')->default('new');
+            $table->timestamp('answered_at')->nullable();
             $table->timestamps();
         });
     }
@@ -25,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('tickets');
     }
 };
